992f1c0bea21b560e6767d50fc2832cbeb7c7c9f
Elyse Erb <jerb@me.com>
2016-08-12 10:18:10 -0700

    Setting up Blocstagram

---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Elyse Erb on 8/12/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
