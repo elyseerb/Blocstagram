90be80a73e23bee2738d28af8a653e1cd38e90ac
Elyse Erb <jerb@me.com>
2016-08-23 17:32:40 -0700

    Setting up for assignment

---
+++ Blocstagram/Blocstagram/Blocstagram/items.h
@@ -0,0 +1,13 @@
+//
+//  items.h
+//  Blocstagram
+//
+//  Created by Elyse Erb on 8/22/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface items : NSObject
+
+@end
---
+++ Blocstagram/Blocstagram/Blocstagram/items.m
@@ -0,0 +1,13 @@
+//
+//  items.m
+//  Blocstagram
+//
+//  Created by Elyse Erb on 8/22/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "items.h"
+
+@implementation items
+
+@end
